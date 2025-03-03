95742936e3cfccfedf544278708f9a0c9cdecb4f
Ambiya12 <ambiyadms@gmail.com>
2025-03-03 11:22:06 +0100

    add more colored console logs for additional names

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ console.log(chalk.red('Daphnée'));
 console.log(chalk.blue('Fadia'));
 console.log(chalk.green('David'));
 console.log(chalk.yellow('Coralie'));
+console.log(chalk.magenta('Toni'));
+console.log(chalk.cyan('Julie'));
+console.log(chalk.white('Lucie'));
+console.log(chalk.gray('Maxime'));
+console.log(chalk.red('Marie')); 
