6678e2b5c1e7b3af46d66ca129c9ab9bff4e5afe
Ambiya12 <ambiyadms@gmail.com>
2025-03-03 14:37:29 +0100

    add HTTP server and file handling functionality

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
 import chalk from 'chalk';
+import http from 'http';
+import fs from 'fs';
+
+const PORT = 8000;
+const HOST = 'localhost';
+const FILE_PATH = './hello.txt';
+
+fs.writeFile(FILE_PATH, 'Hello World!', (err) => {
+    if (err) {
+        console.error('Error writing file:', err);
+        return;
+    }
+    console.log('File has been created');
+
+    fs.readFile(FILE_PATH, 'utf-8', (err, data) => {
+        if (err) {
+            console.error('Error reading file:', err);
+            return;
+        }
+        console.log('File Content:', data);
+    });
+});
 
 console.log(chalk.red('Daphnée'));
 console.log(chalk.blue('Fadia'));
@@ -8,4 +30,26 @@ console.log(chalk.magenta('Toni'));
 console.log(chalk.cyan('Julie'));
 console.log(chalk.white('Lucie'));
 console.log(chalk.gray('Maxime'));
-console.log(chalk.red('Marie')); 
+console.log(chalk.red('Marie'));
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<h1>Hello World!</h1>');
+});
+
+server.listen(PORT, HOST, () => {
+    console.log(`Server is running on http://${HOST}:${PORT}`);
+});
+
+
+/*
+
+const server = http.createServer((req, res) => {
+  res.end('Welcome to my first HTTP server');
+})
+
+server.listen(PORT, localhost, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+*/
